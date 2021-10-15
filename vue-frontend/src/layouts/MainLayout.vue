ce70cb92706d74c75e1b5cb29a02dd3e26cd422e
Forrester <d>
2021-10-15 13:51:42 -0700

    Created project and removed boilerplate code

---
+++ vue-frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,85 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer()"
+        />
+
+        <q-toolbar-title> SweetPapa's Apex Chart Demo </q-toolbar-title>
+
+        <div>Built with Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" bordered>
+      <img
+        class="q-pa-sm q-mt-md"
+        alt="SPC logo"
+        src="~assets/spc.png"
+        style="width: 200px; height: auto"
+      />
+      <q-list>
+        <q-item-label header> Navigation </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Home",
+    caption: "Homepage",
+    icon: "home",
+    link: "/",
+  },
+    {
+    title: "Chart Dashboard",
+    caption: "Demo of Apex Charts",
+    icon: "graph",
+    link: "/",
+  },
+];
+
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+    };
+  },
+  methods: {
+    toggleLeftDrawer() {
+      this.leftDrawerOpen = !this.leftDrawerOpen;
+    },
+  },
+});
+</script>
---
+++ vue-frontend/src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="bg-image flex flex-center">
+    <div style="min-width: 100%; min-height: 100%">
+      <div class="">
+        <center>
+        <img
+          class="col q-pt-xl"
+          alt="Sweet Papa Consulting Marketing Image"
+          src="~assets/spc-mid-logo.png"
+          style="width: 50vw; height: auto; max-width: 500px; user-drag: none; -moz-user-select: none; -webkit-user-drag: none;
+"
+        />
+        <div class="col text-h1 text-white">Apex Charts Demo</div>
+        <div class="col text-h5 text-white">by Forrester Terry <a class="text-blue text-h6" style="text-decoration:none;" href="https://github.com/SweetPapa">(@SweetPapa)</a></div>
+
+        </center>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
+<style lang="scss" scoped>
+.bg-image {
+  background-image: url("https://firebasestorage.googleapis.com/v0/b/spc-apexchart-demo.appspot.com/o/assets%2FSnow-night.gif?alt=media&token=7890a56d-84e4-43c8-864e-f963c7c92fe0");
+  background-repeat: no-repeat;
+  background-size: cover;
+  box-shadow: inset 0 0 0 2000px rgba(56, 55, 53, 0.8);
+}
+</style>
